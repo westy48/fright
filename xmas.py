361bc43a1a0b2e087556a7dad239199642b0ce3b
James West <westy48@gmail.com>
2016-12-05 22:11:54 +0000

    Update xmas.py

--- xmas.py
+++ xmas.py
@@ -4,9 +4,12 @@ import pygame.mixer
 from pygame.mixer import Sound
 import time
 from signal import pause
+import random
+import os
 
 pygame.mixer.init()
-santa = Sound("Laugh.wav")
+randomfile = ""
+file = ""
 
 # Set the GPIO pins
 led = PWMLED(24)
@@ -14,7 +17,12 @@ button = Button(3)
 
 while True:
     button.wait_for_press()
-    led.pulse(n=5)
+    time.sleep(0.25)
+    led.pulse(n=10)
+    randomfile = random.sample(os.listdir("/home/pi/fright/music/"), 1)[0]
+    print randomfile
+    file = '/home/pi/fright/music/'+ randomfile
+    santa = Sound(file)
+    print file
+    pygame.mixer.music.set_volume(0.5)
     santa.play()
-
-
